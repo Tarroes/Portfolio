ee85237f494939aeb57ff8dfdda5b821018ab439
Tarroes <Tarroes@yahoo.com>
2026-09-03 23:59:50 -0400

    Add basic transaction entry and validation

--- mainMenu.py
+++ mainMenu.py
@@ -3,13 +3,37 @@ OnMainMenu = True
 def mainMenu():
     global OnMainMenu
     if mainMenuSelection == "1":
-        print("Runs function: addTransaction()")
+        addTransaction()
     elif mainMenuSelection == "2":
         print("Runs function: viewTransactions()")
     elif mainMenuSelection == "3":
         OnMainMenu = False
     else:
         print("Invalid selection. Please try again.")
+
+def addTransaction():
+    ValidTransDescription = False
+    ValidTransAmount = False
+
+    while ValidTransDescription == False:
+        transDescription = input("Please enter a short (20 characters or less) description of the transaction:")
+        if len(transDescription) > 20:
+            print("Description is too long. Please try again.")
+        else:
+            ValidTransDescription = True
+
+    while ValidTransAmount == False:
+        transAmount = input("Please enter the amount of the transaction:")
+        try:
+            transAmount = float(transAmount)
+            ValidTransAmount = True
+        except ValueError:
+            print("Invalid amount. Please enter a valid number.")
+
+    print("Transaction added successfully!")
+    print("Description: " + transDescription)
+    print("Amount: " + str(transAmount))
+    
 
 while OnMainMenu == True:
     print("Please select an option from the menu:")
